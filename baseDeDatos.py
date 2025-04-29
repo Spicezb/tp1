566643df1adca794f218b522180be71c37230bd9
Luisguialfa <luisguialfa@gmail.com>
2025-04-29 01:45:56 -0600

    sdcgsdfgsfg

--- baseDeDatos.py
+++ baseDeDatos.py
@@ -1,1 +1,18 @@
 #Hola cabrones
+# pene de negro que rico
+# playo, pero se puede hacer todo en este mismo archivo? o like en 2 diferentes, como el del lab digamos
+
+def pMenu():
+    return("1. Crear BD dinámica\n" \
+        "2. Registrar un estudiante\n" \
+        "3. Generar reporte HTML y .csv \n" \
+        "4. Respaldar en XML \n" \
+        "5. Reporte por género (.docx) \n" \
+        "6. Gestionar curva \n" \
+        "7. Envió de correos para reposición. \n" \
+        "8. Aplazados en al menos 2 exámenes (.pdf) \n" \
+        "9. Estadística por generación \n" \
+        "10. Reporte por sede con buen rendimiento. \n11. Salir")
+print(pMenu)
+
+#negro
