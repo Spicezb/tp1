1912bc9735a87d79deb052fc40931ae1771a68ce
Luisguialfa <luisguialfa@gmail.com>
2025-04-30 11:23:19 -0600

    Empece a hacer la funcion de crear carnets

--- funciones.py
+++ funciones.py
@@ -1,26 +1,29 @@
-# <<<<<<< HEAD
-# # def agregarEstudiante(nombre,carnet,sede,genero,correo,apellidos):
-
-
-def agregarEstudianteES():
-    nombre="juan perez rojas"
-    carnet="4321"
-    # sede=
-    # genero=
-    correo=f"{nombre[0]}{nombre[nombre.find(" ")+1:nombre.find(" ",nombre.find(" ")+1)]}{carnet[-4:]}@estudiantec.cr"
-    return correo
-
-print(agregarEstudianteES())
-#
 # Trabajo realizado Por Luis Guillermo Alfaro Chacón y Xavier Céspedes Alvarado.
 # Fecha de inicio: 29/04/2025 a las 12:00
 #
 # Versión de python: 3.13.2
+# <<<<<<< HEAD
+# # def agregarEstudiante(nombre,carnet,sede,genero,correo,apellidos):
+
+
 # Importación de librerias
 import names
 import random
 import pickle
 # funciones
+def crearCarne():
+    return
+def crearCarneAux(ini,fin):
+    return crearCarne()
+def crearCarneES(x):
+    if x == True:
+        print("Dijite el rango de años para generar los carnets")
+        rango=input("Año inicial: ")
+        rango2=input("Año final: ")
+        return crearCarneAux(rango, rango2)
+    else:
+        return
+
 def crearNombres():
     """
     Funcionamiento:
@@ -42,6 +45,9 @@ def crearNombres():
     else:
         genero= False
     nombrePersona = (persona, pA, sA)
+    carne= crearCarne()
+    correo= crearCorreo(True)
+    notas= agregarNotas()
     infoPerso.append(nombrePersona)
     infoPerso.append(genero)
     return(infoPerso)
@@ -52,3 +58,13 @@ def crearBD(archivo,lista):
     pickle.dump(lista,lol)
     lol.close()
     return
+
+def agregarEstudianteES():
+    nombre="juan perez rojas"
+    carnet="4321"
+    # sede=
+    # genero=
+    correo=f"{nombre[0]}{nombre[nombre.find(" ")+1:nombre.find(" ",nombre.find(" ")+1)]}{carnet[-4:]}@estudiantec.cr"
+    return correo
+
+print(agregarEstudianteES())
