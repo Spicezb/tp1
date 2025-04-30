a68c56962bf856aa433512dd2187017576e31535
Luisguialfa <luisguialfa@gmail.com>
2025-04-30 01:20:52 -0600

    Se hace la funcion de crear la base de baseDeDatos Se hace la funcion de crear los nombres random.

--- funciones.py
+++ funciones.py
@@ -1,2 +1,40 @@
+# Trabajo realizado Por Luis Guillermo Alfaro Chacón y Xavier Céspedes Alvarado.
+# Fecha de inicio: 29/04/2025 a las 12:00
+#
+# Versión de python: 3.13.2
+# Importación de librerias
+import names
+import random
+import pickle
 # funciones
+def crearNombres():
+    """
+    Funcionamiento:
+    Se crean nombres de manera aleatoria, gracias a la libreia names y random, donde se le asigna un genero al nombre,
+    y se crea el nombre dependiendo del genero dado. Luego la informacion se almacena por separado.
+    Entradas:
+    N/A
+    Salidas:
+    Se retorna una lista con una tupla que contiene el nombre y los apellidos, y el genero por aparte, este de manera Booleana
+    """
+    nombrePersona=()
+    infoPerso=[]
+    genero=random.choice(("male","female"))
+    persona=names.get_first_name(gender=genero)
+    pA=names.get_last_name()
+    sA=names.get_last_name()
+    if genero=="male":
+        genero = True
+    else:
+        genero= False
+    nombrePersona = (persona, pA, sA)
+    infoPerso.append(nombrePersona)
+    infoPerso.append(genero)
+    return(infoPerso)
 
+def crearBD(archivo,lista):
+    lol=open(archivo,"wb")
+    lista.append(crearNombres())
+    pickle.dump(lista,lol)
+    lol.close()
+    return
