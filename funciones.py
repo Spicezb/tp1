172e91af53d2b7b8fab26c52ebcf7102369f4ee9
Spicezb <xaviccr@gmail.com>
2025-04-30 01:28:10 -0600

    crear correo

--- funciones.py
+++ funciones.py
@@ -1,2 +1,13 @@
-# funciones
+# def agregarEstudiante(nombre,carnet,sede,genero,correo,apellidos):
+
+
+def agregarEstudianteES():
+    nombre="juan perez rojas"
+    carnet="4321"
+    # sede=
+    # genero=
+    correo=f"{nombre[0]}{nombre[nombre.find(" ")+1:nombre.find(" ",nombre.find(" ")+1)]}{carnet[-4:]}@estudiantec.cr"
+    return correo
+
+print(agregarEstudianteES())
 
